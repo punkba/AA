40849f9c9419614ab48bb62a3ef39e6b5beb7811
punkba <prasadhsai07@gmail.com>
2018-11-23 11:42:45 +0530

    Add files via upload

--- R/modelling_module.R
+++ R/modelling_module.R
@@ -64,7 +64,8 @@ modelling_module<-function(DV,model_selection,predictorClass)
         
         modelCoeff <- data.table(summary(model)$coefficients,keep.rownames = T)
         modelCoeff$pval <-  modelCoeff[,5]
-        modelCoeff<-  modelCoeff[pval<0.1][,colnames(modelCoeff[,c(3,4,5,6)]):=NULL]
+        modelCoeff <- modelCoeff[modelCoeff$pval < 0.1]
+        modelCoeff<-  modelCoeff[,colnames(modelCoeff[,c(3,4,5,6)]):=NULL]
       }
       
       modelCoeff <-list(modelCoeff=I(modelCoeff))
