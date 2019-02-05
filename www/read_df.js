020b5f9ce8d3c74e5b6be3198a61a76f2dd4e00a
punkba <prasadhsai07@gmail.com>
2019-02-05 16:52:13 +0530

    Add files via upload

--- www/read_df.js
+++ www/read_df.js
@@ -1,16 +1,83 @@
 $(document).ready(function(){
   
+	var categorical = '';
+	var continuous = '';
+	var discrete = '';
+	
+function createList(value,locToCreate){
+	var node = document.createElement("LI");           		
+	var textnode = document.createTextNode(value);
+	node.appendChild(textnode);
+	document.getElementById(locToCreate).appendChild(node);
+}
+	
+function createCheckBox(value,elem){
+	var chk = document.createElement('input');  // CREATE CHECK BOX.
+    chk.setAttribute('type', 'checkbox');       // SPECIFY THE TYPE OF ELEMENT.
+    chk.setAttribute('id', 'Variable'+elem);     // SET UNIQUE ID.
+    chk.setAttribute('value', value);
+    chk.setAttribute('name', 'variables');
+	
+	var lbl = document.createElement('label');  // CREATE LABEL.
+    lbl.setAttribute('for', value + ' Box');
+
+    // CREATE A TEXT NODE AND APPEND IT TO THE LABEL.
+    lbl.appendChild(document.createTextNode(obj.value));
+	
+	discreteBox.appendChild(chk);
+	discreteBox.appendChild(lbl);
+	
+	value = '';
+}
+	
+	$("#submitbutton").on("click", function(){
+		var filename = $("#uploadFile")[0].files[0];
+		dvname=$("#dvname").val();
+		preddv=$("#preddv").val();
+		ds=$("#data-split").val();
+	 
+		if(!filename){
+			alert("No file selected.");
+			return;
+		}
+		
+		if(!dvname){
+		  alert("Please provide DV name.");
+		  return;
+		}
+		
+		if(!preddv){
+		  alert("Please provide DV class name.");
+		  return;
+		}
+		
+		if(!ds){
+		  alert("Please provide split ratio name.");
+		  return;
+		}
+		
+		$("#status1").text("Reading the CSV...");	
+	  
+		uploadcsv(filename); 
+	  });
+	});	
+  
   function uploadcsv(filename){
-	 //alert("inside uploadcsv....");
-    //disable the button during upload
-    $("#submitbutton").attr("disabled", "disabled");  
-    //perform the request
+    $("#submitbutton").attr("disabled", "disabled");
     var req = ocpu.call("read_csv", {
-      file : filename
-    }, function(session){
-		$("#status1").text("Wait till I clean it for you!");
-      //on success call printsummary()
-      printsummary(session);
+		file : filename
+    },function(session){
+		$("#status1").text("Analysing the Data !!");
+		session.getObject(function(full_output){
+			getAndDisplayVariables(full_output);
+		}).fail(
+		function(){
+			alert("Server error: " + req.responseText);
+		});
+		$("#status1").text("Go to next page for variable information ..");
+		//Focusing to next page automatically
+		$("#chooseCSV").removeClass("section--is-active");
+		$("#varManip").addClass("section--is-active");
     });
     
     //if R returns an error, alert the error message
@@ -24,18 +91,40 @@ $(document).ready(function(){
     });        
   }    
   
-  
+function getAndDisplayVariables(listInput){
+	categorical = listInput[1]['categorical'];
+	continuous = listInput[2]['continuous'];
+	discrete = listInput[3]['discrete'];
+	
+	//create the lsit elements to display continuous variables
+	for(elem = 0;elem < categorical.length;elem++){
+		createList(categorical[elem],'#cate-list');
+	}
+	
+	//create the lsit elements to display continuous variables
+	for(elem = 0;elem < continuous.length;elem++){
+		createList(continuous[elem],'#conte-list');
+	}
+	
+	//create checkbox to convert discrete to categorical
+	for(elem = 0;elem < discrete.length;elem++){
+		createCheckBox(discrete[elem],elem);
+	}
+}
+
+
+
+
+/*  
   function printsummary(mydata){
-	  //alert("inside printsummary....");
-    //perform the request
-    var req = ocpu.call("printsummary", {
-      "df_full" : mydata,
-	"dvname" : dvname,
-	"preddv" : preddv
-    }, function(session){
-      session.getObject(function(output){
-        //$("#output code").text(output);	
-		$("#status1").text("Cleaning Successful! Setting up the results");
+	var req = ocpu.call("printsummary", 
+						{"df_full" : mydata,
+						"dvname" : dvname,
+						"preddv" : preddv
+						}, 
+						function(session){
+							session.getObject(function(output){
+							$("#status1").text("Cleaning Successful! Setting up the results");
 	
 		//FUNCTION CALLS TO SIGNIFICANT VARIABLE LIST & GRAPHS PLOTS
 	  plot_graph();
@@ -45,7 +134,7 @@ $(document).ready(function(){
     });        
   });
   }
-  
+  /*
   
 		//Adding code for var_list call
 	
@@ -137,47 +226,4 @@ $(document).ready(function(){
 				
 			}
 		
-  
-  $("#submitbutton").on("click", function(){
-    
-	//alert("inside script....");
-    //arguments
-    //read the value for 'filename'
-	//var filename = $("#uploadFile").val();
-	  var filename = $("#uploadFile")[0].files[0];
-	  dvname=$("#dvname").val();
-	  preddv=$("#preddv").val();
-	  ds=$("#data-split").val();
-	  
-	  
-    
-    if(!filename){
-      alert("No file selected.");
-      return;
-    }
-	
-	
-	
-	if(!dvname){
-      alert("Please provide DV name.");
-      return;
-    }
-	
-	if(!preddv){
-      alert("Please provide DV class name.");
-      return;
-    }
-	
-	if(!ds){
-      alert("Please provide split ratio name.");
-      return;
-    }
-	
-	$("#status1").text("Reading the CSV...");	
-  
-	uploadcsv(filename); 
-  });
-
-
-  });
-
+  */
--- www/var_change.js
+++ www/var_change.js
@@ -2,88 +2,38 @@ $(document).ready(function(){
 $('#varChangeLnk').hide();
 //Get the variable list after uploading the data
 
-
-var categorical = ;
-var continuous = '';
-var discrete = '';
-
-function getAndDisplayVariables(listInput){
-	categorical = listInput[1]['categorical'];
-	continuous = listInput[2]['continuous'];
-	discrete = listInput[3]['discrete'];
-	
-	//create the lsit elements to display continuous variables
-	for(elem = 0;elem < categorical.length;elem++){
-		createList(categorical[elem],'#cate-list');
-	}
-	
-	//create the lsit elements to display continuous variables
-	for(elem = 0;elem < continuous.length;elem++){
-		createList(continuous[elem],'#conte-list');
-	}
-	
-	//create checkbox to convert discrete to categorical
-	for(elem = 0;elem < discrete.length;elem++){
-		createCheckBox(discrete[elem],elem);
-	}
-}
-
-function createList(value,locToCreate){
-	var node = document.createElement("LI");           		
-	var textnode = document.createTextNode(sig_var[i]);
-	node.appendChild(textnode);
-	document.getElementById(locToCreate).appendChild(node);
-}
-
-function createCheckBox(value,elem){
-	var chk = document.createElement('input');  // CREATE CHECK BOX.
-    chk.setAttribute('type', 'checkbox');       // SPECIFY THE TYPE OF ELEMENT.
-    chk.setAttribute('id', 'Variable'+elem);     // SET UNIQUE ID.
-    chk.setAttribute('value', value);
-    chk.setAttribute('name', 'variables');
-	
-	var lbl = document.createElement('label');  // CREATE LABEL.
-    lbl.setAttribute('for', value + ' Box');
-
-    // CREATE A TEXT NODE AND APPEND IT TO THE LABEL.
-    lbl.appendChild(document.createTextNode(obj.value));
-	
-	discreteBox.appendChild(chk);
-	discreteBox.appendChild(lbl);
-	
-	value = '';
-}
-
 function getUpdatedVariableList(){
 	var checkBoxes = document.getElementById('discreteBox').elements;
-	var updatedDiscrete = '';
+	var elementList = new Array();
 	
 	for(var i=0;checkBoxes.length;i++)
 	{
 		var checkBoxObj = document.getElementById('Variable'+i);
 		if(checkBoxObj.checked == true)
 		{
-			categorical.push(checkBoxObj.value);
+			elementList.push(checkBoxObj.value);
 		}
-		else
-		{
-			updatedDiscrete.push(checkBoxObj.value)
-		}
-	} 
-		
-	var finalOutput  = finalOutput.push(continuous);
-	finalOutput.push(categorical);
-	finalOutput.push(updatedDiscrete);
+	}
+	
+	return elementList;
 }
 
 $('#varChangeBtn').on('click',function(){
 	//Disable the button for the user
 	$('#varChangeBtn').prop("disabled",true);
-	
 	//Get the list of variable names after updation by user for passing it to R
-	
-	
-	var req = ocpu.call("fileName",{},function(session){});
+
+	/*var req = ocpu.call("fileName",
+						{variable:getUpdatedVariableList()},
+						function(session){
+								session.getObject(function(full_output){
+									document.getElementById('varChangeLnk').href = session.getFileURL(full_output[1]['fileName']);
+								}).fail(
+									function(){
+										alert("Server error: " + req.responseText);
+									}
+								);
+						});*/
 	$('#varChangeLnk').show();
 });
 
