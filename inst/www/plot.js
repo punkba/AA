9cdaf79133a42fc596cd860233c745b0bead55bb
Prasadh <sprasadh@paypal.com>
2019-02-11 16:11:35 +0530

    Updated dropdown select logic

--- inst/www/plot.js
+++ inst/www/plot.js
@@ -1,18 +1,7 @@
 $(document).ready(function(){
 	
-	var selectedVar = '';
-	
 	$("#varProfileOptions a").on('click',function(){
-		console.log($(this).text());
-		selectedVar = $(this).text();
-		$('#varDropdownMenuButton').html(selectedVar);
-		
-		/*
-		var req = $('#plotdiv1').rplot('variable_profiling_function',{dv:dvname,var:selectedVar});
-		
-		req.fail(function(){
-				alert("Server error: " + req.responseText);
-		}); */
+	alert(dropDownSelect);
 	});
 });
 
