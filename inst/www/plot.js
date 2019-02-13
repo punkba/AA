39092c24e169135053ba236efe210e44b4aa6233
punkba <prasadhsai07@gmail.com>
2019-02-13 10:30:51 +0530

    Update plot.js
    Included code to call R script when the variable selection is made

--- inst/www/plot.js
+++ inst/www/plot.js
@@ -2,9 +2,11 @@ $(document).ready(function(){
 	var dropDownSelect ='';
 	
 	$("#varProfileOptions").on('click','a',function(){
-		console.log($(this).text());
 		dropDownSelect = $(this).text();
 		$('#varDropdownMenuButton').html(dropDownSelect);
+		
+		//Call the R script to update the plot
+		plotProfilingGraph(dropDownSelect);
 	});
 });
 
