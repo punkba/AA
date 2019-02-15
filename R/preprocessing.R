7a42464c092c60c807d78f0e4b591a9e53faff68
punkba <prasadhsai07@gmail.com>
2019-02-15 13:01:29 +0530

    loaded plyr

--- R/preprocessing.R
+++ R/preprocessing.R
@@ -1,6 +1,7 @@
 preprocessing <- function(conv_var_names, dv){
 
 library(dplyr)
+library(plyr)
 
 data = read.csv(file='C:/opencpuapp_ip/prepro_step1.csv', header=TRUE, sep=",")
 names(data)[names(data)==dv] <- "DV"
