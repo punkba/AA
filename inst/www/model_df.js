741260c1ce6e0108c093710b384e84d5a4bbac28
punkba <prasadhsai07@gmail.com>
2019-02-15 15:41:51 +0530

    Comma

--- inst/www/model_df.js
+++ inst/www/model_df.js
@@ -147,7 +147,7 @@ $(document).ready(function(){
 						    top: 5,
 						    width: '100%',
 						    height: '100%'
-						}
+						},
 						bar: {groupWidth: "50%"},
 						legend: {position:"none"}
 					}
